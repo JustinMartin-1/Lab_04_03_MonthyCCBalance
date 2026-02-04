cbc0d10cd353615a2b504820cea48ee14cbd6f95
Jusitn783 <justin.martin9001@gmail.com>
2026-02-04 14:34:22 -0500

    Lab 04 001

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
+public class Main {
+    public static void main(String[] args) {
 
+        double startingBalance = 5000.0;
+        double interestRate = 0.17;
+
+        double monthOneInterest = startingBalance * interestRate;
+        double monthOneBalance = startingBalance + monthOneInterest;
+
+        double monthTwoInterest = monthOneBalance * interestRate;
+        double monthTwoBalance = monthOneBalance + monthTwoInterest;
+
+        System.out.println("Starting balance: $" + startingBalance);
+        System.out.println("Interest after 1 month: $" + monthOneInterest);
+        System.out.println("Balance after 1 month: $" + monthOneBalance);
+        System.out.println("Interest after 2 months: $" + monthTwoInterest);
+        System.out.println("Balance after 2 months: $" + monthTwoBalance);
     }
-
+}
