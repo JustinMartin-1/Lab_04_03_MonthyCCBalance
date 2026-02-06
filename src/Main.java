e960019b6808d9f9f2784a2f5b575606488d54e4
Jusitn783 <justin.martin9001@gmail.com>
2026-02-06 10:55:26 -0500

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        double startingBalance = 5000.0;
+        double balance = 5000.0;
         double interestRate = 0.17;
 
-        double monthOneInterest = startingBalance * interestRate;
-        double monthOneBalance = startingBalance + monthOneInterest;
+        double monthlyInterest = balance * interestRate;
+        balance += monthlyInterest;
 
-        double monthTwoInterest = monthOneBalance * interestRate;
-        double monthTwoBalance = monthOneBalance + monthTwoInterest;
-
-        System.out.println("Starting balance: $" + startingBalance);
-        System.out.println("Interest after 1 month: $" + monthOneInterest);
-        System.out.println("Balance after 1 month: $" + monthOneBalance);
-        System.out.println("Interest after 2 months: $" + monthTwoInterest);
-        System.out.println("Balance after 2 months: $" + monthTwoBalance);
+        System.out.println("Balance after 1 month: $" + balance);
+        monthlyInterest = balance * interestRate;
+        balance += monthlyInterest;
+        System.out.println("Balance after 2 months: $" + balance);
     }
 }
